13a5f7c046c6d8f68b9f93102de66d3b98ef7acc
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-01-05 18:15:38 -0300

    asddd

--- api/src/routes/paycrypto.js
+++ api/src/routes/paycrypto.js
@@ -57,16 +57,16 @@ route.post("/create-charge", async(req,res)=>{   // ruta de pago http://localhos
     
     for(let i=0; i<ticket.length; i++){
 
-        qrGenerate("hola");
+        qrGenerate({
+            event: name,
+            price: price,
+            typeTicket: typeTicket,
+            usersId: 01,
+            ticketId: ticket[i].id
+        });
     };
     
-    // {
-    //     event: name,
-    //     price: price,
-    //     typeTicket: typeTicket,
-    //     usersId: 01,
-    //     ticketId: ticket[i].id
-    // }
+    
     //-------------------------------------------------------------------------------------------------------
 
     
