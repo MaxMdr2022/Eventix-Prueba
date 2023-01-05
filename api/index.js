688223df5cb1a8873a0f73271a3c2028b20dd08a
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-01-05 20:00:52 -0300

    true

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 // Syncing all the models at once.
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
