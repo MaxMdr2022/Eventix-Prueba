e4ef416d1280c45217dc51b5100718e3b87d1866
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-01-05 22:49:39 -0300

    function-

--- api/src/routes/ticket.js
+++ api/src/routes/ticket.js
@@ -1,5 +1,7 @@
 const {Router} = require("express");
 const getTickets= require("../controllers/getTickets");
+const qrCode = require("qrcode");
+
 
 const route = Router();
 
@@ -11,10 +13,32 @@ route.get("/:userId", async(req,res)=>{
         const {userId} = req.params;
 
 
-        const ticket = await getTickets(Number(userId));
-        console.log("tickets ruta:" ,ticket);
+        // const ticket = await getTickets(Number(userId));
 
-        ticket? res.status(200).json(ticket) : res.status(400).send("No ticket");
+
+        const qrGenerate = async text => {
+
+            try {
+                let qr = await qrCode.toDataURL(text);
+
+    
+                // console.log("ticketfuncion", ticket[0]);
+                console.log("qr::::", qr);
+ 
+                return res.status(200).json(qr)
+    
+    
+            } catch (error) {
+                
+                console.log(error);
+            }
+        };
+
+        qrGenerate("hola")
+
+        // console.log("tickets ruta:" ,ticket);
+
+        // ticket? res.status(200).json(ticket) : res.status(400).send("No ticket");
 
     } catch (error) {
         
